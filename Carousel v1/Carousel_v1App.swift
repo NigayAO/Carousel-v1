faa7bfe642a93ce4d38cea14053fa3045aad680d
Alik <Nigay_AO@bk.ru>
2022-09-04 12:37:42 +0300

    Initial Commit

---
+++ Carousel v1/Carousel_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  Carousel_v1App.swift
+//  Carousel v1
+//
+//  Created by Alik Nigay on 04.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Carousel_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carousel v1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Carousel v1
+//
+//  Created by Alik Nigay on 04.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
